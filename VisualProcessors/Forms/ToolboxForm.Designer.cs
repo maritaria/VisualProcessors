49cf3c218a2d32c131e8bafc39a97cdaf097f0ab
mooshbeef <bramkamies@gmail.com>
2014-10-03 08:43:24 +0200

    Resync-commit

---
+++ VisualProcessors/Forms/ToolboxForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace VisualProcessors.Forms
+{
+	partial class ToolboxForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// ToolboxForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(222, 196);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Name = "ToolboxForm";
+			this.Text = "ToolboxForm";
+			this.Load += new System.EventHandler(this.ToolboxForm_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
