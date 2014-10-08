35c14d698aa99d46c1009b8c57b288767c9acc1f
mooshbeef <bramkamies@gmail.com>
2014-10-08 12:22:59 +0200

    Adding untracked files. Created CSVProcessor

---
+++ ExternalLibraryTest/ExternalProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualProcessors.Processing;
+
+namespace ExternalLibraryTest
+{
+	[ProcessorAttribute("Bram Kamies", "This processor is loaded from an external assembly", "Input", "Output")]
+	public class ExternalProcessor : Processor
+	{
+		public ExternalProcessor()
+		{
+		}
+
+		public ExternalProcessor(string name)
+			: base(name)
+		{
+			AddInputChannel("Input", false);
+			AddOutputChannel("Output");
+		}
+
+		protected override void Process()
+		{
+			GetOutputChannel("Output").WriteValue(GetInputChannel("Input").GetValue());
+		}
+	}
+}
