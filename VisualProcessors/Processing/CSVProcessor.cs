808c3831e21cd8ba208dbb76c35c5d3e7c031de0
mooshbeef <bramkamies@gmail.com>
2014-10-14 13:35:47 +0200

    CSVProcessor now inherits from FileBasedProcessor

--- VisualProcessors/Processing/CSVProcessor.cs
+++ VisualProcessors/Processing/CSVProcessor.cs
@@ -14,14 +14,13 @@ namespace VisualProcessors.Processing
 	[ProcessorMeta("Bram Kamies", "Writes the input to a .csv file", "1", "",
 		CustomTabMode = ProcessorTabMode.Hidden,
 		OutputTabMode = ProcessorTabMode.Hidden)]
-	public class CSVProcessor : Processor
+	public class CSVProcessor : FileBasedProcessor
 	{
 		#region Properties
 
 		private StreamWriter m_FileWriter;
-		private bool m_Loaded = false;
-		private bool m_OutputSet = false;
-		private bool m_Overwrite = true;
+		private bool m_Truncate = false;
+
 
 		#endregion Properties
 
@@ -61,30 +60,7 @@ namespace VisualProcessors.Processing
 
 		[Browsable(true)]
 		[ReadOnly(false)]
-		[DisplayName("Output file")]
-		[Category("Settings")]
-		[Description("The file to write the input data to")]
-		[DefaultValue("")]
-		[Editor(typeof(System.Windows.Forms.Design.FileNameEditor), typeof(System.Drawing.Design.UITypeEditor))]
-		public string FilePath
-		{
-			get
-			{
-				return Options.GetOption("FilePath", "");
-			}
-			set
-			{
-				m_OutputSet = false;
-				ThrowExceptionOnInvalid(value);
-				m_OutputSet = true;
-				Options.SetOption("FilePath", value);
-				OnModified(HaltTypes.ShouldHalt);
-			}
-		}
-
-		[Browsable(true)]
-		[ReadOnly(false)]
-		[DisplayName("Column seperator")]
+		[DisplayName("Seperator")]
 		[Category("Settings")]
 		[Description("The string to seperate columns by, is not added to the last column")]
 		[DefaultValue("")]
@@ -101,6 +77,24 @@ namespace VisualProcessors.Processing
 			}
 		}
 
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Truncate")]
+		[Category("Settings")]
+		[Description("When true, the CSVProcessor will truncate(empty) the target file whenever it runs Prepare(). When false, it will always append to the existing contents of the file")]
+		[DefaultValue(true)]
+		public bool TruncateOnPrepare
+		{
+			get
+			{
+				return bool.Parse(Options.GetOption("TruncateOnReset", "True"));
+			}
+			set
+			{
+				Options.SetOption("TruncateOnReset", value.ToString());
+			}
+		}
+
 		#endregion Options
 
 		#region Constructor
@@ -108,22 +102,23 @@ namespace VisualProcessors.Processing
 		public CSVProcessor()
 			: base()
 		{
-			m_Loaded = true;
+			ExtensionRestriction.Add(".csv");
 		}
 
 		public CSVProcessor(Pipeline pipeline, string name)
 			: base(pipeline, name)
 		{
 			AddInputChannel("1", false);
+			ExtensionRestriction.Add(".csv");
 		}
 
 		#endregion Constructor
 
 		#region Methods
 
-		public override void Dispose()
+		public override void Stop()
 		{
-			base.Dispose();
+			base.Stop();
 			if (m_FileWriter != null)
 			{
 				m_FileWriter.Dispose();
@@ -131,89 +126,24 @@ namespace VisualProcessors.Processing
 			}
 		}
 
-		public override void GetUserInterface(Panel panel)
+		protected override void Prepare()
 		{
+			base.Prepare();
+			m_Truncate = TruncateOnPrepare;
 		}
-
 		public override void Start()
 		{
 			base.Start();
-			ThrowExceptionOnInvalid(FilePath);
-			FileMode mode = FileMode.Append;
-			if (m_Overwrite)
+			if (m_FileWriter!=null)
 			{
-				mode = FileMode.Create;
+				m_FileWriter.Dispose();
 			}
-			m_FileWriter = new StreamWriter(new FileStream(FilePath, mode));
-		}
-
-		public override void Stop()
-		{
-			base.Stop();
-			DisposeFileWriter();
-			m_Overwrite = false;
-		}
-
-		public void ThrowExceptionOnInvalid(string path)
-		{
-			if (path == "")
+			if (m_Truncate)
 			{
-				throw new Exception("No output file selected");
+				FileStream.SetLength(0);
 			}
-			FileInfo info = new FileInfo(path);
-			if (!info.Exists)
-			{
-				File.Create(path).Close();
-				info = new FileInfo(path);
-			}
-			if (info.Extension != ".csv")
-			{
-				throw new Exception("File extension is invalid: must be *.csv");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.ReadOnly) || info.IsReadOnly)
-			{
-				throw new Exception("The selected file is a read-only file");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.Directory))
-			{
-				throw new Exception("The selected file is a directory");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.System))
-			{
-				throw new Exception("The selected file is a system-only file");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.Offline))
-			{
-				throw new Exception("The selected file is offline or unavailable");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.Temporary))
-			{
-				//OnWarning("The selected file is a temporary file, it may be removed when the application closes");
-			}
-			if (info.Attributes.HasFlag(FileAttributes.Compressed))
-			{
-				//OnWarning("The selected file is compressed, unknown behaviour on read/write-operations");
-			}
-
-			//This will try to open the file, and immidiatly close it again.
-			//This will throw an exception if the file is in use by another program.
-			info.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None).Close();
-		}
-
-		protected override void Prepare()
-		{
-			if (m_Loaded)
-			{
-				if (FilePath != "")
-				{
-					m_OutputSet = true;
-				}
-				m_Loaded = false;
-			}
-			if (m_OutputSet)
-			{
-				m_Overwrite = true;
-			}
+			FileStream.Position = FileStream.Length;
+			m_FileWriter = new StreamWriter(FileStream);
 		}
 
 		protected override void Process()
@@ -225,33 +155,11 @@ namespace VisualProcessors.Processing
 				m_FileWriter.Write(GetInputChannel((i + 1).ToString()).GetValue());
 				if (i < c - 1)
 				{
-					//System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator
 					m_FileWriter.Write(Seperator);
 				}
 			}
 			m_FileWriter.WriteLine();
 			m_FileWriter.Flush();
-		}
-
-		protected override void WorkerMethod()
-		{
-			if (!m_OutputSet)
-			{
-				OnWarning("No output set");
-			}
-			else
-			{
-				base.WorkerMethod();
-			}
-		}
-
-		private void DisposeFileWriter()
-		{
-			if (m_FileWriter != null)
-			{
-				m_FileWriter.Dispose();
-				m_FileWriter = null;
-			}
 		}
 
 		#endregion Methods
