27fc2d8986572a3a1a777e4fc0a1fd7639ff8c06
mooshbeef <bramkamies@gmail.com>
2014-10-14 16:44:49 +0200

    Added seperate thread for TcpClient. (+CodeMaid)

--- VisualProcessors/Processing/TcpClientProcessor.cs
+++ VisualProcessors/Processing/TcpClientProcessor.cs
@@ -1,31 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace VisualProcessors.Processing
 {
+	[ProcessorMeta("Bram Kamies", "A base processor for processors which use a TcpClient", "", "",
+		AllowUserSpawn = false)]
 	public class TcpClientProcessor : Processor
 	{
 		#region Properties
 
-		public static string HostnameRegex = @"([a-zA-Z0-9]([a-zA-Z0-9\-]*\.?[a-zA-Z0-9\-])*\.[a-zA-Z0-9\-]*[a-zA-Z0-9])";
-		public static string IpAddressRegex = @"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})";
-		public static string PortTailRegex = @"(\:[0-9]{1-5})";
+		public static string HostnameRegex = @"^([a-zA-Z0-9]([a-zA-Z0-9\-]*\.?[a-zA-Z0-9\-])*\.[a-zA-Z0-9\-]*[a-zA-Z0-9])|(localhost)$";
+		public static string IpAddressRegex = @"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$";
 
 		private TcpClient m_TcpClient;
 		private NetworkStream m_TcpStream;
+		private Thread m_TcpThread;
+
+		protected TcpClient TcpClient
+		{
+			get
+			{
+				return m_TcpClient;
+			}
+		}
+
+		protected NetworkStream TcpStream
+		{
+			get
+			{
+				return m_TcpStream;
+			}
+		}
 
 		#endregion Properties
 
 		#region Options
 
-
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Hostname")]
+		[Category("Settings")]
+		[Description("The IP-address or DNS-hostname to connect to")]
+		[DefaultValue("localhost")]
 		public string Hostname
 		{
 			get
@@ -34,20 +58,22 @@ namespace VisualProcessors.Processing
 			}
 			set
 			{
-				if (value.Contains(":"))
-				{
-					throw new ArgumentException("Hostname cannot contain a port devider (:) ");
-				}
-				if (!(Regex.IsMatch(value,HostnameRegex)||Regex.IsMatch(value,IpAddressRegex)))
+				if (!(Regex.IsMatch(value, HostnameRegex) || Regex.IsMatch(value, IpAddressRegex)))
 				{
 					throw new Exception("Hostname does conform to a valid IP-Address or DNS hostname");
 				}
-				
+
 				Options.SetOption("Hostname", value);
 				OnModified(HaltTypes.Ask);
 			}
 		}
 
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Port")]
+		[Category("Settings")]
+		[Description("The port to connect to, must be in the range of 0-65535")]
+		[DefaultValue(1000)]
 		public int Port
 		{
 			get
@@ -69,26 +95,17 @@ namespace VisualProcessors.Processing
 			}
 		}
 
-		public bool ConnectInWorkerThread
-		{
-			get
-			{
-				return bool.Parse(Options.GetOption("ConnectInWorkerThread", "False"));
-			}
-			set
-			{
-				Options.GetOption("ConnectInWorkerThread", value.ToString());
-				OnModified(HaltTypes.Continue);
-			}
-		}
-
 		#endregion Options
 
 		#region Constructor
-		public TcpClientProcessor() : base()
+
+		public TcpClientProcessor()
+			: base()
 		{
 		}
-		public TcpClientProcessor(Pipeline p, string name) :base(p,name)
+
+		public TcpClientProcessor(Pipeline p, string name)
+			: base(p, name)
 		{
 		}
 
@@ -98,61 +115,15 @@ namespace VisualProcessors.Processing
 
 		public override void Start()
 		{
-			if (m_TcpClient!=null)
+			if (m_TcpThread != null)
 			{
-				DisposeTcpClient();
+				m_TcpThread.Abort();
+				while (m_TcpThread.IsAlive) ;
 			}
-			if (!ConnectInWorkerThread)
-			{
-				Connect();
-			}
+			m_TcpThread = new Thread(new ThreadStart(TcpWorkerThread));
+			m_TcpThread.IsBackground = true;
+			m_TcpThread.Start();
 			base.Start();
-		}
-		protected override void WorkerMethod()
-		{
-			if (ConnectInWorkerThread)
-			{
-				Connect();
-			}
-			byte[] buffer;
-			int read = 0;
-			OutputChannel channel = GetOutputChannel("Data");
-			while(true)
-			{
-				buffer = new byte[m_TcpClient.ReceiveBufferSize];
-				try
-				{
-					read = m_TcpStream.Read(buffer, 0, m_TcpClient.ReceiveBufferSize);
-				}
-				catch(Exception e)
-				{
-					OnWarning("Connection lost: " + e.Message);
-					return;
-				}
-				if (read==0)
-				{
-					OnWarning("Connection lost: socket closed");
-					return;
-				}
-				if (HandlePacket(buffer,read))
-				{
-					DisposeTcpClient();
-					return;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Overridable function for packet handling implementation.
-		/// </summary>
-		/// <param name="packet">The packet that has been received, may be incomplete</param>
-		/// <param name="length">The number of bytes read from the network stream, any bytes in the packet array beyond length should be ignored</param>
-		/// <returns>True if the WorkerThread should disconnect</returns>
-		protected virtual bool HandlePacket(byte[] packet, int length)
-		{
-			Console.WriteLine("TcpClientProcessor.HandlePacket():");
-			Console.WriteLine(BitConverter.ToString(packet, 0, length));
-			return false;
 		}
 
 		public override void Stop()
@@ -161,15 +132,16 @@ namespace VisualProcessors.Processing
 			DisposeTcpClient();
 		}
 
-		protected void Connect()
+		protected void Connect(string hostname, int port)
 		{
-			if (m_TcpClient!=null)
+			if (m_TcpClient != null)
 			{
 				DisposeTcpClient();
 			}
-			m_TcpClient = new TcpClient(Hostname, Port);
+			m_TcpClient = new TcpClient(hostname, port);
 			m_TcpStream = m_TcpClient.GetStream();
 		}
+
 		protected void DisposeTcpClient()
 		{
 			if (m_TcpClient != null)
@@ -178,6 +150,55 @@ namespace VisualProcessors.Processing
 				m_TcpClient = null;
 				m_TcpStream.Dispose();
 				m_TcpStream = null;
+			}
+		}
+
+		/// <summary>
+		///  Overridable function for packet handling implementation.
+		/// </summary>
+		/// <param name="packet">The packet that has been received, may be incomplete</param>
+		/// <param name="length">
+		///  The number of bytes read from the network stream, any bytes in the packet array beyond
+		///  length should be ignored
+		/// </param>
+		/// <returns>True if the WorkerThread should disconnect</returns>
+		protected virtual bool HandlePacket(byte[] packet, int length)
+		{
+			return false;
+		}
+
+		protected void TcpWorkerThread()
+		{
+			if (m_TcpClient != null)
+			{
+				DisposeTcpClient();
+			}
+			Connect(Hostname, Port);
+			byte[] buffer;
+			int read = 0;
+			OutputChannel channel = GetOutputChannel("Data");
+			while (true)
+			{
+				buffer = new byte[m_TcpClient.ReceiveBufferSize];
+				try
+				{
+					read = m_TcpStream.Read(buffer, 0, m_TcpClient.ReceiveBufferSize);
+				}
+				catch (Exception e)
+				{
+					OnWarning("Connection lost: " + e.Message);
+					return;
+				}
+				if (read == 0)
+				{
+					OnWarning("Connection lost: socket closed");
+					return;
+				}
+				if (HandlePacket(buffer, read))
+				{
+					DisposeTcpClient();
+					return;
+				}
 			}
 		}
 
