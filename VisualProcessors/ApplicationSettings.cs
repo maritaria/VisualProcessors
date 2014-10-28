8a6baa38f37af8d60cdc20977a452a818bddddb8
mooshbeef <bramkamies@gmail.com>
2014-10-28 08:28:39 +0100

    Added meta for dipslay in a PropertyGrid

--- VisualProcessors/ApplicationSettings.cs
+++ VisualProcessors/ApplicationSettings.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.ComponentModel;
 
 namespace VisualProcessors.Forms
 {
@@ -12,24 +13,72 @@ namespace VisualProcessors.Forms
 	{
 		#region Properties
 
+		[Browsable(false)]
+		[ReadOnly(true)]
 		public string LastOpenedPipelineFile { get; set; }
 
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Open last file opened on startup")]
+		[Category("Startup")]
+		[Description("When true, the application will (on startup) load up the file that was opened last by the application previously")]
+		[DefaultValue(false)]
 		public bool OpenLastPipelineOnStartup { get; set; }
 
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Open last file opened on startup")]
+		[Category("Startup")]
+		[Description("When true, the application will (on startup) open the data form")]
+		[DefaultValue(false)]
 		public bool ShowDataFormOnStartup { get; set; }
 
 		[XmlElement(Type = typeof(XmlColor))]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Simulation color")]
+		[Category("Worksheet")]
+		[Description("Sets the background color of the worksheet during simulation")]
 		public Color SimulationBackgroundColor { get; set; }
-
+		
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Start maximized")]
+		[Category("Startup")]
+		[Description("When true, the application will (on startup) maximize itself")]
+		[DefaultValue(true)]
 		public bool StartMaximized { get; set; }
 
 		[XmlElement(Type = typeof(XmlColor))]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Default color")]
+		[Category("Worksheet")]
+		[Description("Sets the background color of the worksheet during editing")]
 		public Color WorksheetBackgroundColor { get; set; }
-
+		
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Pipeline")]
+		[Category("File extensions")]
+		[Description("Sets the file extension used to save/load pipeline files")]
+		[DefaultValue(".pipe.xml")]
 		public string PipelineFileExtension { get; set; }
-
+		
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Channel data")]
+		[Category("File extensions")]
+		[Description("Sets the file extension used to save/load channel data")]
+		[DefaultValue(".cdata.xml")]
 		public string ChannelDataFileExtension { get; set; }
-
+		
+		[Browsable(true)]
+		[ReadOnly(false)]
+		[DisplayName("Assembly directory")]
+		[Category("Startup")]
+		[Description("Sets the directory in the CWD to load assemblies from, requires restart to take effect")]
+		[DefaultValue("/Assemblies")]
 		public string AssemblySubDirectory { get; set; }
 
 		#endregion Properties
@@ -41,12 +90,12 @@ namespace VisualProcessors.Forms
 			LastOpenedPipelineFile = "";
 			OpenLastPipelineOnStartup = false;
 			ShowDataFormOnStartup = false;
-			SimulationBackgroundColor = Color.Pink;
+			SimulationBackgroundColor = Color.Crimson;
 			StartMaximized = true;
 			WorksheetBackgroundColor = SystemColors.ActiveCaption;
 			PipelineFileExtension = ".pipe.xml";
 			ChannelDataFileExtension = ".cdata.xml";
-			AssemblySubDirectory = "/Assemblies";
+			AssemblySubDirectory = "Assemblies";
 		}
 
 		#endregion Constructor
